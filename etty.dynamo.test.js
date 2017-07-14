bbf34a794a56bdeff9a4eb2879ee94a5345d594d
Robert Hill <dr.pappa@gmail.com>
2017-07-14 21:58:08 +0100

    lint

--- etty.dynamo.test.js
+++ etty.dynamo.test.js
@@ -1,68 +1,69 @@
+/*global describe, it, beforeEach */
 const assert = require('chai').assert;
 const sinon = require('sinon');
 const ettyDynamo = require('./etty.dynamo');
 
 describe('etty.dynamo', function() {
-	let etty;
-	let client;
-	const term = 'ill-gotten';
-	const successResponse = {
-		Count: 1,
-		Items: [
-			{
-				pos: 'n',
-				etymology: 'word etymology'
-			}
-		]
-	};
-	const noResults = {
-		Count: 0
-	};
-	const error = {
-		message: ''
-	};
+    let etty;
+    let client;
+    const term = 'ill-gotten';
+    const successResponse = {
+        Count: 1,
+        Items: [
+            {
+                pos: 'n',
+                etymology: 'word etymology'
+            }
+        ]
+    };
+    const noResults = {
+        Count: 0
+    };
+    const error = {
+        message: ''
+    };
 
-	beforeEach(function() {
-		etty = ettyDynamo(client);
-		client = {
-			search: sinon.stub()
-		};
-	});
+    beforeEach(function() {
+        etty = ettyDynamo(client);
+        client = {
+            search: sinon.stub()
+        };
+    });
 
-	describe('search()', function() {
+    describe('search()', function() {
 
-		it('should return a successful response', function(done) {
-			client.search.yields(null, successResponse);
-			done();
+        it('should return a successful response', function(done) {
+            client.search.yields(null, successResponse);
+            done();
 
-			etty.search(term, function(err, result) {
-				assert.isNull(err);
-				assert.equal(result, 'ill-gotten - n - word etymology');
-				done();
-			});
-		});
+            etty.search(term, function(err, result) {
+                assert.isNull(err);
+                assert.equal(result, 'ill-gotten - n - word etymology');
+                done();
+            });
+        });
 
-		it('should return no results', function(done) {
-			client.search.yields(null, noResults);
-			done();
+        it('should return no results', function(done) {
+            client.search.yields(null, noResults);
+            done();
 
-			etty.search(term, function(err, result) {
-				assert.isNull(err);
-				assert.equal(result, 'Sorry, no results.');
-				done();
-			});
-		});
+            etty.search(term, function(err, result) {
+                assert.isNull(err);
+                assert.equal(result, 'Sorry, no results.');
+                done();
+            });
+        });
 
-		it('should return an error', function(done) {
-			client.search.yields(error, {});
-			done();
+        it('should return an error', function(done) {
+            client.search.yields(error, {});
+            done();
 
-			etty.search(term, function(err, result) {
-				assert.isNotNull(err);
-				done();
-			});
-		});
+            etty.search(term, function(err) {
+                assert.isNotNull(err);
+                done();
+            });
+        });
 
-	});
+    });
 
 });
