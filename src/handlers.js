dda12a585e5c028c397249254f2e3c99a5b813b2
Robert Hill <dr.pappa@gmail.com>
2017-08-24 21:07:47 +0100

    LaunchIntent

--- src/handlers.js
+++ src/handlers.js
@@ -4,7 +4,9 @@ const APP_ID = process.env.APP_ID;
 module.exports = function(etty) {
     return {
         'LaunchRequest': function () {
-            this.emitWithState('AMAZON.HelpIntent');
+            this.attributes.speechOutput = this.t('LAUNCH_MESSAGE');
+            this.attributes.repromptSpeech = this.t('LAUNCH_MESSAGE');
+            this.emit(':ask', this.attributes.speechOutput, this.attributes.repromptSpeech);
         },
         'Ask': function () {
             const word = this.event.request.intent.slots.word;
