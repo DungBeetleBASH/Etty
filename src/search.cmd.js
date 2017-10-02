4fdcf2b5fe3f52c1ebf880028bd4fdb03290423b
Robert Hill <dr.pappa@gmail.com>
2017-10-02 20:24:48 +0100

    WIP - switching to new search

---
+++ src/search.cmd.js
@@ -0,0 +1,14 @@
+'use strict';
+/*eslint no-console:0*/
+const AWS = require('aws-sdk');
+AWS.config.update({region:'eu-west-1'});
+const client = new AWS.DynamoDB.DocumentClient();
+const etty = require('./etty.dynamo2')(client);
+const term = process.argv.slice(2).join(' ');
+
+console.log(term);
+
+etty.search(term, function(err, result) {
+    console.log(JSON.stringify(err, null, 4));
+    console.log(JSON.stringify(result, null, 4));
+});
